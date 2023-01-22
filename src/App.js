01dd1f6bfc34c187d1433d67956e6e35a2055a2f
Danish <dan71ish@gmail.com>
2023-01-22 11:12:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Home from "./pages/homePage"
+
+const App = () => {
+  return <div><Home /></div>
+}
+
+export default App;
---
+++ src/pages/homePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection'
+const home = () => {
+    return <HeroSection />
+}
+
+export default home;
