4b297c9194f1561b203ae8541637e109ac94d1cd
Danish <dan71ish@gmail.com>
2023-01-25 17:01:13 +0530

    Revert "scaliing animation added"
    This reverts commit a454afb3f689325bf629414f38a7bba29be518e9.
    
    jlfsljkf# Please enter the commit message for your changes. Lines starting

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
-import ProductShowcase from '../components/ProductShowcase';
 const HomePage = () => {
     return (
     <>
     <Header />
     <HeroSection />   
-    <ProductShowcase />
     </>
     );
 }
