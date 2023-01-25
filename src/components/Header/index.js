a892c109b179101e6407818123b27fc41792f5bd
Danish <dan71ish@gmail.com>
2023-01-25 15:48:54 +0530

    hamburger component done deploy state

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import './header.css';
-const header = () => {
-    const [showMobMenu,setShowMobMenu] = useState(false);
-    const toggleMobileMenu =()=>{
+
+ const Header = () => {
+    const [showMobMenu,setShowMobMenu] = useState(false); 
+    const toggleMobileMenu = () => {
         setShowMobMenu(!showMobMenu);
     }
     return (
         <div className="mobile-menu-wrapper">
-            <div className={`mobile-menu only-mobile ${showMobMenu ? 'overlay': ''}`}>
+            <div className={`mobile-menu only-mobile ${showMobMenu ? "overlay": ""}`}>
                 <div className='mobile-navbar'>
                 <div className='mobile-nav-item'>credit score check</div>
                     <div className='mobile-nav-item'>CRED pay</div>
@@ -16,7 +17,7 @@ const header = () => {
             <div className="flex max-width header">
                 <img className="header-logo" src="https://web-images.credcdn.in/_next/assets/images/home-page/cred-logo.png" alt="Cred logo" />
                 <div className="only-mobile mobile-menu-button-wrapper">
-                    <button class={`hamburger hamburger--elastic ${showMobMenu ?"is-active":""}`} type="button" onClick={toggleMobileMenu}>
+                    <button class={`hamburger hamburger--elastic ${showMobMenu ?"is-active":""}`} type="button" onClick={ toggleMobileMenu }>
                         <span class="hamburger-box">
                             <span class="hamburger-inner"></span>
                         </span>
@@ -28,6 +29,6 @@ const header = () => {
                 </div>
             </div>
         </div>
-    )
-}
-export default header;
+    );
+};
+export default Header;
