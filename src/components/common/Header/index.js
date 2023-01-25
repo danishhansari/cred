f37f15181694338bdff6d9e6677832127570722f
Danish <dan71ish@gmail.com>
2023-01-25 11:32:26 +0530

    hamburger component done

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './header.css'
+import React, { useState } from 'react';
+import './header.css';
 const header = () => {
+    const [showMobMenu,setShowMobMenu] = useState(false);
+    const toggleMobileMenu =()=>{
+        setShowMobMenu(!showMobMenu);
+    }
     return (
         <div className="mobile-menu-wrapper">
+            <div className={`mobile-menu only-mobile ${showMobMenu ? 'overlay': ''}`}>
+                <div className='mobile-navbar'>
+                <div className='mobile-nav-item'>credit score check</div>
+                    <div className='mobile-nav-item'>CRED pay</div>
+                </div>
+            </div>
             <div className="flex max-width header">
-                <img className="header-logo" src="https://web-images.credcdn.in/_next/assets/images/home-page/cred-logo.png" alt="CRed logo" />
+                <img className="header-logo" src="https://web-images.credcdn.in/_next/assets/images/home-page/cred-logo.png" alt="Cred logo" />
                 <div className="only-mobile mobile-menu-button-wrapper">
-                    <button class="hamburger hamburger--elastic" type="button">
+                    <button class={`hamburger hamburger--elastic ${showMobMenu ?"is-active":""}`} type="button" onClick={toggleMobileMenu}>
                         <span class="hamburger-box">
                             <span class="hamburger-inner"></span>
                         </span>
