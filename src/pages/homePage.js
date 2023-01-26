50c5302701a979c766a3d190a37d6da1b901994c
Danish <dan71ish@gmail.com>
2023-01-26 12:29:25 +0530

    mobile component added

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
+import Productshowcase from '../components/Productshowcase'
 const HomePage = () => {
     return (
     <>
     <Header />
     <HeroSection />   
+    <Productshowcase />
     </>
     );
 }
